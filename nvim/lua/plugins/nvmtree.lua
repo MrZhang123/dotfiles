09b19439858132446edce0defe3633b6301ea019
raymond <mark_zhang1991@qq.com>
2023-10-10 10:57:13 +0800

    feat: rm not use config

--- nvim/lua/plugins/nvmtree.lua
+++ nvim/lua/plugins/nvmtree.lua
@@ -17,7 +17,6 @@ return {{
         view = {
             width = 30,
             -- height = 30,
-            hide_root_folder = false,
             side = "left",
             preserve_window_proportions = false,
             number = false,
